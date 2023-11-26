88344e1d142a071aad8803561119a8fa2d8a8ad4
MichelleeileneWSU <151972116+MichelleeileneWSU@users.noreply.github.com>
2023-11-26 12:16:20 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -11,13 +11,29 @@ pygame.display.set_caption("Space Invaders 581")
 icon = pygame.image.load('alien.png')
 pygame.display.set_icon(icon)
 
+#player
+playerImg = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+def player(x,y):
+    screen.blit(playerImg, (x, y))
+
 # game loop
 running = True
 while running:
+
+    # RGB
+    screen.fill((74, 103, 65))
+
+    playerY -= 0.1
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # RGB
-    screen.fill((74, 103, 65))
+    player(playerX, playerY)
     pygame.display.update()
+
+

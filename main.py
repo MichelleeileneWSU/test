bb8ab8d4cd4eab80bb27a0592e607074bc479cf1
MichelleeileneWSU <151972116+MichelleeileneWSU@users.noreply.github.com>
2023-11-26 10:24:35 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import pygame
 
+#Initialize the pygame
+pygame.init()
 
+#create screen
+screen = pygame.display.set_mode((800,600))
+
+# title and icon
+pygame.display.set_caption("Space Invaders 581")
+icon = pygame.image.load('alien.png')
+pygame.display.set_icon(icon)
+
+# game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # RGB
+    screen.fill((74, 103, 65))
+    pygame.display.update()

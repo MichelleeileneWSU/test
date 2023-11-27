6b4ae65b892e7ef383022c5bd81797c30d4802f1
MichelleeileneWSU <151972116+MichelleeileneWSU@users.noreply.github.com>
2023-11-27 09:28:46 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -27,12 +27,22 @@ while running:
     # RGB
     screen.fill((74, 103, 65))
 
-    playerY -= 0.1
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    #if keystroke is pressed, check right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                 playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
     player(playerX, playerY)
     pygame.display.update()
 
